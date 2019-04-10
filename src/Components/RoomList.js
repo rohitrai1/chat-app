9602886f59ecb8de0182bea9bc57ea22ad2db0f2
rovit <rohit47099@gmail.com>
2019-04-10 23:30:26 +0530

    CSS improvements

--- src/Components/RoomList.js
+++ src/Components/RoomList.js
@@ -6,26 +6,32 @@ class RoomList extends Component {
   render() {
     const orderedRooms = [...this.props.rooms].sort((a, b) => a.id - b.id);
     return (
-      <div className="row roomlist">
-        <div className="offset-sm-2">
-          <h2>Rooms:</h2>
-          {orderedRooms.map((room, index) => (
-            <li
-              key={index}
-              className={
-                this.props.roomId === room.id
-                  ? "navigation-active"
-                  : "navigation"
-              }
-            >
-              <button
-                id="link"
-                onClick={() => this.props.subscribeToRoom(room.id)}
+      <div className="roomlist">
+        <div className="row">
+          <div className="col-12">
+            <h2>Rooms:</h2>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-12">
+            {orderedRooms.map((room, index) => (
+              <li
+                key={index}
+                className={
+                  this.props.roomId === room.id
+                    ? "navigation-active"
+                    : "navigation"
+                }
               >
-                {room.name}
-              </button>
-            </li>
-          ))}
+                <button
+                  id="link"
+                  onClick={() => this.props.subscribeToRoom(room.id)}
+                >
+                  {room.name}
+                </button>
+              </li>
+            ))}
+          </div>
         </div>
       </div>
     );
